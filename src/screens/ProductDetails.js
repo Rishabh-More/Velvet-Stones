b4856fc20f8a20ce70da9ac1184b2fc570fd6176
Rishabh More <rishabh.more@inviziosolutions.com>
2020-09-19 18:47:03 +0530

    Setting up React Navigation Part 2: Created Screen Templates

---
+++ src/screens/ProductDetails.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import useDeviceOrientation from "@react-native-community/hooks";
+import { isTablet, isPhone } from "react-native-device-detection";
+import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+
+export default function ProductDetails() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>This is Product Details Screen</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
